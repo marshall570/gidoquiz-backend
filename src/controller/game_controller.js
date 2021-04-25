a5846ed6db5fc1216d0609415368a14e2f756f12
Gustavo Fernandes <marshall@arch.localdomain>
2021-04-25 18:19:10 -0300

    Remove old SQL files

--- src/controller/game_controller.js
+++ src/controller/game_controller.js
@@ -4,7 +4,7 @@ module.exports = {
     async insert(request, response) {
         const { pergunta, alt_a, alt_b, alt_c, alt_d, resposta, equipe } = request.body
 
-        const last_question = await Pergunta.countDocuments({equipe: equipe})
+        const last_question = await Pergunta.countDocuments({ equipe: equipe })
         console.log(last_question)
         const numero = last_question + 1
 
@@ -32,7 +32,7 @@ module.exports = {
 
     async total(request, response) {
         const { tn } = request.query
-        const query_result = await Pergunta.countDocuments({equipe: tn})
+        const query_result = await Pergunta.countDocuments({ equipe: tn })
 
         return response.json({ total_perguntas: query_result })
     },
